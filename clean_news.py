62927b2a9caec51d91414c6e509a2705428095c2
wychen_huawei <wychen0524@aliyun.com>
2023-11-24 13:12:52 +0800

    modify the output of cleaned_data

--- clean_news.py
+++ clean_news.py
@@ -3,8 +3,9 @@ import json
 import re
 
 # 创建一个空的字典，用于存储所有的数据
-data = {}
 root = "securityNewsData"
+month_list1 = ['07', '08', '09', '10', '11', '12']
+month_list2 = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11']
 
 
 # 判断是否包含韩语
@@ -24,22 +25,54 @@ def clean_text(text):
 if __name__ == "__main__":
     # 遍历文件夹中的所有文件
     file_list = os.listdir(root)
-    for filename in file_list:
-        if filename.endswith('.json'):
-            path = './' + root + '/' + filename
-            with open(path, 'r', encoding='UTF-8') as f:
-                # 读取文件内容并转换为Python字典
-                news_list = json.load(f)
-                for news_json in news_list:
-                    if contains_korean(str(news_json)) is False:
-                        key = (news_json['time'], news_json['title'])
-                        news_json['title'] = clean_text(news_json['title'])
-                        news_json['text'] = clean_text(news_json['text'])
-                        data[key] = news_json
-    json_list = list(data.values())
-    print(len(json_list))
-    sorted_list = sorted(json_list, key=lambda x: x['time'], reverse=True)
+    for month in month_list1:
+        data_2022 = {}
+        for filename in file_list:
+            if filename.split('_')[1] == '2022' and filename.split('_')[2] == month:
 
-    # 将字典转换为列表，然后写入新的JSON文件
-    with open('output/output.json', 'w', encoding='utf-8') as f:
-        json.dump(sorted_list, f, indent=4, ensure_ascii=False)
+                path = './' + root + '/' + filename
+                with open(path, 'r', encoding='UTF-8') as f:
+                    # 读取文件内容并转换为Python字典
+                    news_list = json.load(f)
+                    for news_json in news_list:
+                        if contains_korean(str(news_json)) is False:
+                            key = (news_json['time'], news_json['title'])
+                            news_json['title'] = clean_text(news_json['title'])
+                            news_json['text'] = clean_text(news_json['text'])
+                            data_2022[key] = news_json
+        json_list = list(data_2022.values())
+        print(len(json_list))
+        sorted_list = sorted(json_list, key=lambda x: x['time'], reverse=True)
+        # 将字典转换为列表，然后写入新的JSON文件
+        with open('output/cleaned_data_2022_' + month, 'w', encoding='utf-8') as f:
+            json.dump(sorted_list, f, indent=4, ensure_ascii=False)
+
+    for month in month_list2:
+        data_2023 = {}
+        for filename in file_list:
+            if filename.split('_')[1] == '2023' and filename.split('_')[2] == month:
+                path = './' + root + '/' + filename
+                with (open(path, 'r', encoding='UTF-8') as f):
+                    # 读取文件内容并转换为Python字典
+                    news_list = json.load(f)
+                    for news_json in news_list:
+                        if contains_korean(str(news_json)) is False:
+                            if month == str(news_json['time']).split('-')[1]:
+                                key = (news_json['time'], news_json['title'])
+                                news_json['title'] = clean_text(news_json['title'])
+                                news_json['text'] = clean_text(news_json['text'])
+                                data_2023[key] = news_json
+        json_list = list(data_2023.values())
+        print(len(json_list))
+        sorted_list = sorted(json_list, key=lambda x: x['time'], reverse=True)
+        # 将字典转换为列表，然后写入新的JSON文件
+        with open('output/cleaned_data_2023_' + month, 'w', encoding='utf-8') as f:
+            json.dump(sorted_list, f, indent=4, ensure_ascii=False)
+
+
+
+
+
+
+
+
